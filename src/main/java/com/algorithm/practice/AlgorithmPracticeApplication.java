7fb390811ffed024708cb11cffe637e8dfb0257b
keweizhou <1514991931@qq.com>
2020-03-18 23:44:27 +0800

    add algorithm-practice

---
+++ src/main/java/com/algorithm/practice/AlgorithmPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.algorithm.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmPracticeApplication.class, args);
+    }
+
+}
