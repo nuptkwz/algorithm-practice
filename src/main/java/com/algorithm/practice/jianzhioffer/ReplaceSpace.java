7f38ded60b126943bd8d9fccf3ceae7810b89457
keweizhou <1514991931@qq.com>
2020-03-21 21:11:02 +0800

    change template

--- src/main/java/com/algorithm/practice/jianzhioffer/ReplaceSpace.java
+++ src/main/java/com/algorithm/practice/jianzhioffer/ReplaceSpace.java
@@ -1,11 +1,11 @@
 package com.algorithm.practice.jianzhioffer;
 
 /**
- * Description:
+ * Description
  * 请实现一个函数，将一个字符串中的空格替换成“%20”。
  * 例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
- * Date 2020/3/19 23:51
- * Created by kwz
+ * Author  kwz
+ * date 2020/3/21 21:04
  */
 public class ReplaceSpace {
 
@@ -15,10 +15,9 @@ public class ReplaceSpace {
     }
 
     /**
-     * 用String内置函数处理
-     *
-     * @param str：input str
-     * @return
+     * Description 用String内置函数处理
+     * Param [str]
+     * return java.lang.String
      */
     private static String handleWithReplace(StringBuffer str) {
         String string = str.toString();
