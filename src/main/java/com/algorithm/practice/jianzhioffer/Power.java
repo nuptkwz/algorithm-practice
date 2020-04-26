281ab960d1a984e609ec5179cbb134fd263777d6
keweizhou <1514991931@qq.com>
2020-04-26 23:03:28 +0800

    add Power

---
+++ src/main/java/com/algorithm/practice/jianzhioffer/Power.java
@@ -0,0 +1,42 @@
+package com.algorithm.practice.jianzhioffer;
+
+/**
+ * Description
+ * 给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方。
+ * 保证base和exponent不同时为0
+ * Date 2020/4/26 22:44
+ * Created by kwz
+ */
+public class Power {
+    public static void main(String[] args) {
+        double base = 2;
+        int exponent = -3;
+        System.out.println("handleWithMathPowMethod:" + handleWithMathPowMethod(base, exponent));
+        System.out.println(handleWithMultiply(2, -3));
+    }
+
+    /**
+     * 利用Math.pow的内置函数处理
+     * Description
+     * Param [base, exponent]
+     * return double
+     */
+    private static double handleWithMathPowMethod(double base, int exponent) {
+        return Math.pow(base, exponent);
+    }
+
+    /**
+     * Description
+     * 用累计乘法
+     * Param [base, exponent]
+     * return double
+     */
+    private static double handleWithMultiply(double base, int exponent) {
+        int sum = 1;
+        int times = Math.abs(exponent);
+        for (int i = 1; i <= times; i++) {
+            sum *= base;
+        }
+        return exponent > 0 ? sum : (1.0 / sum);
+    }
+}
