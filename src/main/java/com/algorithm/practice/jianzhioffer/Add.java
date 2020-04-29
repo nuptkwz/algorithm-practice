01afd3dd265d2b26a868e4f0f63f1b426bc0408c
keweizhou <1514991931@qq.com>
2020-04-29 23:56:25 +0800

    add SumSolution

---
+++ src/main/java/com/algorithm/practice/jianzhioffer/Add.java
@@ -0,0 +1,16 @@
+package com.algorithm.practice.jianzhioffer;
+
+/**
+ * Description
+ * 写一个函数，求两个整数之和，要求在函数体内不得使用+、-、*、/四则运算符号。
+ * Date 2020/4/28 22:55
+ * Created by kwz
+ */
+public class Add {
+    public static void main(String[] args) {
+        int a = 3, b = 5;
+        System.out.println(a^b<<1);
+    }
+
+
+}
---
+++ src/main/java/com/algorithm/practice/jianzhioffer/SumSolution.java
@@ -0,0 +1,17 @@
+package com.algorithm.practice.jianzhioffer;
+
+/**
+ * Description
+ * 求1+2+3+...+n，要求不能使用乘除法、
+ * for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+ * Date 2020/4/28 22:50
+ * Created by kwz
+ */
+public class SumSolution {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
