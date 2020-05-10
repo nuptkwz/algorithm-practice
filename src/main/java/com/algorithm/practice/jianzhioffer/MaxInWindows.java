1779514b96b8e22c7e5ff21fb4ca169cc849a248
keweizhou <1514991931@qq.com>
2020-05-10 15:39:12 +0800

    add MaxInWindows

--- src/main/java/com/algorithm/practice/jianzhioffer/MaxInWindows.java
+++ src/main/java/com/algorithm/practice/jianzhioffer/MaxInWindows.java
@@ -1,6 +1,8 @@
 package com.algorithm.practice.jianzhioffer;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 /**
@@ -16,25 +18,57 @@ import java.util.PriorityQueue;
 public class MaxInWindows {
 
     public static void main(String[] args) {
-
-
+        int[] arr = {2, 3, 4, 2, 6, 2, 5, 1};
+        int size = 3;
+        System.out.println(maxInWindowsWithPriorityQueue(arr, size));
     }
 
-    public static ArrayList<Integer> maxInWindows(int[] num, int size) {
-        ArrayList<Integer> list = new ArrayList<>();
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
+    /**
+     * Description
+     * 使用优先队列，大顶堆来解决
+     * Param [arr, size]
+     * return java.util.ArrayList<java.lang.Integer>
+     */
+    private static ArrayList<Integer> maxInWindowsWithPriorityQueue(int[] arr, int size) {
+        if (arr.length <= 0 || size <= 0) {
+            return new ArrayList<>();
+        }
+        ArrayList<Integer> maxInWindowList = new ArrayList<>();
+        PriorityQueue<Integer> queue = new PriorityQueue<>(11,
+                (o1, o2) -> o2 - o1
+        );
         int index = 0;
-        while (index + size < num.length) {
+        while (index + size - 1 < arr.length) {
             int curIndex = index;
             int curSize = size;
-            while (curSize>0){
-                queue.offer(num[curIndex+curSize-1]);
+            while (curSize > 0) {
+                queue.offer(arr[curIndex + curSize - 1]);
                 curSize--;
             }
             Integer peek = queue.peek();
-            list.add(peek);
+            maxInWindowList.add(peek);
             queue.clear();
             index++;
+        }
+        return maxInWindowList;
+    }
+
+    /**
+     * Description
+     * 利用arrayList来实现
+     * Param [num, size]
+     * return java.util.ArrayList<java.lang.Integer>
+     */
+    public ArrayList<Integer> handleWithArrayList(int[] num, int size) {
+        ArrayList<Integer> list = new ArrayList<>();
+        if (num == null || num.length == 0 || size > num.length || size == 0) return list;
+        for (int i = 0; i + size <= num.length; i++) {
+            int temp = num[i];
+            for (int j = i + 1; j < i + size; j++) {
+                if (num[j] > temp)
+                    temp = num[j];
+            }
+            list.add(temp);
         }
         return list;
     }
